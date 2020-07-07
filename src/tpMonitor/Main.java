7e46a0948f39aa4bf6943697430143ec66470a81
Adriel <adriel.chagay@gmail.com>
2020-07-06 21:39:53 -0300

    asdasdasa

--- src/tpMonitor/Main.java
+++ src/tpMonitor/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("sadas");
+		int[][] incidence = {{-1,-1,0,0,1,1},{1,0,0,-1,0,0},{0,1,-1,0,0,0},{0,0,0,1,-1,0},{0,0,1,0,0,-1}};
+		int[][] marking = {{0,0,0,0},{0,0,0,0}};
+		int[] transitions = {0,0,0,0};
+ 		
+		
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 6; j++) {
+				System.out.print(incidence[i][j]+" ");
+			}
+			System.out.println("");
+		}
 	}
-
 }

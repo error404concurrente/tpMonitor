3dd3f535679745aa13d0480a710717efb47aeac0
Adriel <adriel.chagay@gmail.com>
2020-07-06 21:42:17 -0300

    asdasdasa2

--- src/tpMonitor/Main.java
+++ src/tpMonitor/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[][] incidence = {{-1,-1,0,0,1,1},{1,0,0,-1,0,0},{0,1,-1,0,0,0},{0,0,0,1,-1,0},{0,0,1,0,0,-1}};
-		int[][] marking = {{0,0,0,0},{0,0,0,0}};
-		int[] transitions = {0,0,0,0};
+		int[][] incidence = {{-1,-1,0,0,1,1},{1,0,-1,0,0,0},{0,1,0,-1,0,0},{0,0,1,0,-1,0},{0,0,0,1,0,-1}};
+		int[] marking = {1,0,0,0};
+		int[] transitions = {1,1,0,0,0,0};
  		
 		
 		for (int i = 0; i < 5; i++) {

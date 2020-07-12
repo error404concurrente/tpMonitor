0a370d1712f7d33f9da72b28183178facc297a47
Adriex <adrielchagay@gmail.com>
2020-07-12 19:01:01 -0300

    anda

--- src/tpMonitor/Main.java
+++ src/tpMonitor/Main.java
@@ -24,16 +24,41 @@ public class Main {
 		Monitor monitor = new Monitor(cantHilos, rdp);
 		
 		//CReacion hilo
-		int[] tarea = {1,0,0,0,0,0};
-		int[] tarea2 = {0,1,0,0,0,0};
-		Hilo hilo = new Hilo(monitor, tarea);
+		int[] tarea = {0,0,0,0,0,0};
+		for (int i = 0; i < 6; i++) {
+			tarea[i]=1;
+			Hilo hilo = new Hilo(monitor, tarea);
+			Thread hilito = new Thread(hilo, "hilito"+i);
+			hilito.start();
+			tarea[i]=0;
+		}
+		
+		int[] tarea0 = {1,0,0,0,0,0};
+		int[] tarea1 = {0,1,0,0,0,0};
+		int[] tarea2 = {0,0,1,0,0,0};
+		int[] tarea3 = {0,0,0,1,0,0};
+		int[] tarea4 = {0,0,0,0,1,0};
+		int[] tarea5 = {0,0,0,0,0,1};
+		Hilo hilo0 = new Hilo(monitor, tarea0);
+		Hilo hilo1 = new Hilo(monitor, tarea1);
 		Hilo hilo2 = new Hilo(monitor, tarea2);
-		Thread hilito = new Thread(hilo, "hilito");
+		Hilo hilo3 = new Hilo(monitor, tarea3);
+		Hilo hilo4 = new Hilo(monitor, tarea4);
+		Hilo hilo5 = new Hilo(monitor, tarea5);
+		Thread hilito0 = new Thread(hilo0, "hilito0");
+		Thread hilito1 = new Thread(hilo1, "hilito1");
 		Thread hilito2 = new Thread(hilo2, "hilito2");
+		Thread hilito3 = new Thread(hilo3, "hilito3");
+		Thread hilito4 = new Thread(hilo4, "hilito4");
+		Thread hilito5 = new Thread(hilo5, "hilito5");
 		
 		
-		hilito.start();
+		hilito0.start();
+		hilito1.start();
 		hilito2.start();
+		hilito3.start();
+		hilito4.start();
+		hilito5.start();
 		
 	}
 }

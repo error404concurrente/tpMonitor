0bd9da24e0b6432e88a5036765998ba2bf0abfda
JimenaVega <jimevega28@gmail.com>
2020-07-13 16:50:51 -0300

    Resize instance of threads

--- src/tpMonitor/Main.java
+++ src/tpMonitor/Main.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.security.Timestamp;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class Main {
 	private static final SimpleDateFormat sdf = new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss");
@@ -35,33 +36,42 @@ public class Main {
 //			hilito.start();
 //			tarea[i]=0;
 //		}
-		
-		int[] tarea0 = {1,0,0,0,0,0};
-		int[] tarea1 = {0,1,0,0,0,0};
-		int[] tarea2 = {0,0,1,0,0,0};
-		int[] tarea3 = {0,0,0,1,0,0};
-		int[] tarea4 = {0,0,0,0,1,0};
-		int[] tarea5 = {0,0,0,0,0,1};
-		Hilo hilo0 = new Hilo(monitor, tarea0);
-		Hilo hilo1 = new Hilo(monitor, tarea1);
-		Hilo hilo2 = new Hilo(monitor, tarea2);
-		Hilo hilo3 = new Hilo(monitor, tarea3);
-		Hilo hilo4 = new Hilo(monitor, tarea4);
-		Hilo hilo5 = new Hilo(monitor, tarea5);
-		Thread hilito0 = new Thread(hilo0, "hilito0");
-		Thread hilito1 = new Thread(hilo1, "hilito1");
-		Thread hilito2 = new Thread(hilo2, "hilito2");
-		Thread hilito3 = new Thread(hilo3, "hilito3");
-		Thread hilito4 = new Thread(hilo4, "hilito4");
-		Thread hilito5 = new Thread(hilo5, "hilito5");
-		
-		
-		hilito0.start();
-		hilito1.start();
-		hilito2.start();
-		hilito3.start();
-		hilito4.start();
-		hilito5.start();
+       
+//		int[] tarea0 = {1,0,0,0,0,0};
+//		int[] tarea1 = {0,1,0,0,0,0};
+//		int[] tarea2 = {0,0,1,0,0,0};
+//		int[] tarea3 = {0,0,0,1,0,0};
+//		int[] tarea4 = {0,0,0,0,1,0};
+//		int[] tarea5 = {0,0,0,0,0,1};
+		final int N_TAREAS = 6;
+		int [][] tareas = {{1,0,0,0,0,0},{0,1,0,0,0,0},{0,0,1,0,0,0},{0,0,0,1,0,0},{0,0,0,0,1,0},{0,0,0,0,0,1}};
+		ArrayList <Thread> hilito = new ArrayList<Thread>();
+		for(int i=0; i<N_TAREAS;i++) {
+			
+			hilito.add(new Thread(new Hilo(monitor, tareas[i]),"hilito "+i));
+			hilito.get(i).start();
+		}
+	
+//		Hilo hilo0 = new Hilo(monitor, tarea0);
+//		Hilo hilo1 = new Hilo(monitor, tarea1);
+//		Hilo hilo2 = new Hilo(monitor, tarea2);
+//		Hilo hilo3 = new Hilo(monitor, tarea3);
+//		Hilo hilo4 = new Hilo(monitor, tarea4);
+//		Hilo hilo5 = new Hilo(monitor, tarea5);
+//		Thread hilito0 = new Thread(hilo0, "hilito0");
+//		Thread hilito1 = new Thread(hilo1, "hilito1");
+//		Thread hilito2 = new Thread(hilo2, "hilito2");
+//		Thread hilito3 = new Thread(hilo3, "hilito3");
+//		Thread hilito4 = new Thread(hilo4, "hilito4");
+//		Thread hilito5 = new Thread(hilo5, "hilito5");
+//		
+//		
+//		hilito0.start();
+//		hilito1.start();
+//		hilito2.start();
+//		hilito3.start();
+//		hilito4.start();
+//		hilito5.start();
 		
 	}
 }

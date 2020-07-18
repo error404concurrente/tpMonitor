19f3559d80ec1d433e56e391ddd0041ed9cb2aa8
Adriex <adrielchagay@gmail.com>
2020-07-18 14:17:39 -0300

    CONflictos solucionados

--- src/tpMonitor/Monitor.java
+++ src/tpMonitor/Monitor.java
@@ -5,113 +5,56 @@ import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.concurrent.Semaphore;
 
 public final class Monitor {
+	//TODO reemplazar Thread por lo que sea que usemos
 	private RedDePetri rdp;
-<<<<<<< HEAD
-	private Queue<Hilo> espera;
-	
-	private int size;
-	
-=======
 	private Cola colaEspera;
 	private int size;
-	
 
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
+
 	private Semaphore entrada;
 	private Semaphore mutex;
 
-	
+
 	public Monitor(int max, RedDePetri red) {
 		rdp = red;
-<<<<<<< HEAD
-		espera = new ConcurrentLinkedDeque<Hilo>();
-=======
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
 		size = max;
 		entrada = new Semaphore(1,true);
 		mutex = new Semaphore(1,true);
-		
+
 		colaEspera = new Cola(rdp,entrada);
 	}
-	
+
 	public void enter(Hilo hilo) throws InterruptedException{
 		// Obtencion de los semaforos
-		Log.spit("Monitor: Entra el hilo " + Thread.currentThread().getName()+" a Cola de Entrada");
-		entrada.acquire();
-		Log.spit("Monitor: Sale el hilo " + Thread.currentThread().getName()+" de Cola de Entrada");
-		mutex.acquire();
-		Log.spit("Monitor: El hilo " + Thread.currentThread().getName()+" tiene exclusion mutua");
+		try {
+			Log.spit("Hilo entrante: "+Thread.currentThread().getName());
+			entrada.acquire();
+			
+			mutex.acquire();
+		} catch (InterruptedException e) {
+			Log.spit("ERROR DE ENTRADA DE MONITOR AIUDA");
+			e.printStackTrace();
+		}
 		execute(hilo);
-		Log.spit("Monitor: El hilo "+Thread.currentThread().getName()+" ha finalizado y se retira del monitor ");
+		Log.spit("TERMINE ME VOY " + Thread.currentThread().getName()+"  Disparo: "+hilo.strTarea());
 	}
-	
+
 	private void execute(Hilo hilo) throws InterruptedException {
-<<<<<<< HEAD
-		if (rdp.disparar(hilo)) {
-			//Se disparo correctamente la transicion
-			Log.spit("Monitor: Confirmacion de Disparo recibida - Se procede a revisar los hilos en la cola de espera");
-=======
-		
 		if ( rdp.verificarCompatibilidad(hilo.getTarea(),hilo) ) {
 			rdp.disparar(hilo);
 			Log.spit("ES COMPATIBLE");
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
 			mutex.release();
 			colaEspera.buscarEspera();
 		} else {
-			//No se disparo la transicion
-			Log.spit("Monitor: Denegacion de Disparo confirmada - Se procede a mandar "+Thread.currentThread().getName()+" a la cola de espera");
-			
+			Log.spit("NO ES COMPATIBLE-PA la espera");
 			mutex.release();
 			entrada.release();
-<<<<<<< HEAD
-			// espera(); //En caso que pregunten antes de irse a dormir
-			encolar(espera, hilo);
-			Log.spit("Monitor: Sale el hilo " + Thread.currentThread().getName()+" de Cola de Espera y esta listo para disparar");
-=======
 			//colaEspera.espera(); //En caso que pregunten antes de irse a dormir
 			colaEspera.encolar(hilo);
 			Log.spit("ME VOY A EJECUTAR " + Thread.currentThread().getName()+"  Disparo: "+hilo.strTarea());
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
 			mutex.acquire();
 			execute(hilo);
 
 		}
 	}
-<<<<<<< HEAD
-
-	private void espera() throws InterruptedException {
-		Log.spit("************** Verificacion Cola de Espera **************");
-		boolean encontrado = false;
-		for (Hilo hilito : espera) {
-			if (rdp.verificarCompatibilidad(hilito.getTarea())) {
-				// ver politica
-				Log.spit("Se ha encontrado un hilo que puede ejecutar su disparo");
-				espera.remove(hilito);
-				Log.spit("************ Fin Verificacion Cola de Espera ************");
-				synchronized (hilito) {
-					hilito.notify();
-				}
-				encontrado = true;
-				break;
-			}
-		}
-		if (!encontrado) {
-			Log.spit("No hay hilos en cola de espera que puedan ejecutar un disparo actualmente");
-			Log.spit("************ Fin Verificacion Cola de Espera ************");
-			entrada.release();
-		}
-	}
-	
-	private void encolar(Queue<Hilo> cola, Hilo hilo) throws InterruptedException {
-		Log.spit("Monitor: Entra el hilo " + Thread.currentThread().getName() + " a la Cola de Espera");
-		cola.add(hilo);
-		synchronized (hilo) {
-			hilo.wait();
-		}
-	}
-
-=======
-	
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
 }
--- src/tpMonitor/RedDePetri.java
+++ src/tpMonitor/RedDePetri.java
@@ -10,49 +10,36 @@ public class RedDePetri {
 	private int [] marcaInicial;
 	private int [] marcaActual;
 
-	
-	
+
+
 	public RedDePetri(int[][] inc, int[] trans, int[] act) {
 		matrizIncidencia= inc;
 		tranSensibilizadas = trans;
 		marcaInicial = act;
 		marcaActual = act;
-		
+
 		Log.spit("P-invariantes");
 		farkasAlgorithm(concatMatrix(inc,getidentityMatrix(inc.length)),inc.length,inc[0].length);
-		
+
 		Log.spit("T-invariantes");
 		int [][] mT =getTranspuesta(inc);
 		farkasAlgorithm(concatMatrix(mT,getidentityMatrix(mT.length)),mT.length,mT[0].length);
-	}	
-	
+	}
+
 	public void changeCurrent(int[] newCurrent) {
 		marcaActual = newCurrent;
 	}
-	
+
 	public boolean disparar(Hilo hilo) {
-<<<<<<< HEAD
-		Log.spit("RdP: Analizando compatibilidad de "+Thread.currentThread().getName()+"  Disparo: {"+hilo.strTarea()+"}");
-		if(verificarCompatibilidad(hilo.getTarea())) {
-			Log.spit("RdP: Compatibilidad Confirmada a "+Thread.currentThread().getName());
-=======
 		Log.spit("Hilo entrante: "+Thread.currentThread().getName()+"  Disparo: "+hilo.strTarea());
 //		if(verificarCompatibilidad(hilo.getTarea(),hilo)) {
 			Log.spit("Compatibilidad Confirmada");
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
 //			Log.spit("Hilo entrante: "+Thread.currentThread().getName()+"  Disparo: "+hilo.strTarea());
 			Log.spit("-------------- Resultados de Disparo --------------");
 			Log.spit("Estado de RdP Antes:   "+strMarcaActual()+"  ----  T. Sensibles Antes:   "+strTranSensible());
 			calcularMarcaActual(hilo.getTarea());
 			calcularVectorSensible();
 			Log.spit("Estado de RdP Despues: "+strMarcaActual()+"  ----  T. Sensibles Despues: "+strTranSensible());
-<<<<<<< HEAD
-			Log.spit("------------------ Fin Resultados -----------------");
-			return true;
-		}else {
-			Log.spit("RdP: Compatibilidad Denegada a "+Thread.currentThread().getName());
-			return false;
-=======
 			Log.spit("-------------- Fin Resultados --------------");
 			Politicas.aumentar(hilo);
 			return true;
@@ -61,7 +48,7 @@ public class RedDePetri {
 //			return false;
 //		}
 	}
-	
+
 	public boolean verificarCompatibilidad(int[] tarea,Hilo hilo){
 		//Ej: verificarCompatibilidad(hilo.getTarea())
 		for(int i = 0; i < tranSensibilizadas.length; i++) {
@@ -71,12 +58,11 @@ public class RedDePetri {
 				return true;
 				}
 			}
->>>>>>> 17541e27aa5a0455b0b9875729b83f2e52515390
 		}
 		Log.spit("Compatibilidad Denegada");
 		return false;
 	}
-	
+
 	public boolean verificarCompatibilidad(int[] tarea){
 		//Ej: verificarCompatibilidad(hilo.getTarea())
 		boolean compatible = false;
@@ -88,7 +74,7 @@ public class RedDePetri {
 		}
 		return compatible;
 	}
-	
+
 	private void calcularMarcaActual(int[] disparo){
 		int aux;
 		for (int i = 0; i < matrizIncidencia.length; i++) {
@@ -99,17 +85,17 @@ public class RedDePetri {
 			marcaActual[i]+=aux;
 		}
 	}
-	
+
 	private void calcularVectorSensible(){
 		int[] s = new int[matrizIncidencia[0].length];  //vector auxiliar S
-		
+
 		for (int i = 0; i < matrizIncidencia[0].length; i++) { //Recorrer columnas
 			for (int j = 0; j < matrizIncidencia.length; j++) { //Recorrer filas
 				s[j] = marcaActual[j] + matrizIncidencia[j][i];	//Calculo de S
 			}
 			/**Crear nuevo vector de transiciones sensibilizadas
 			 * En la posicion i va a ser 1 si no hay valores negativos en S,
-			 * de otra forma será 0 
+			 * de otra forma será 0
 			 */
 			tranSensibilizadas[i]=1;
 			for (int x = 0; x < s.length; x++) {
@@ -122,7 +108,7 @@ public class RedDePetri {
 	}
 
 	private void farkasAlgorithm(int[][] concat, int rowInc, int colInc) {
-		
+
 		ArrayList<Integer> regPivot = new ArrayList<Integer>();
 		int pivot = 0, pivotNum = 0, factor = 0;
 		boolean notFind = true;
@@ -166,10 +152,10 @@ public class RedDePetri {
 		//printMatrix(invariants);
 		Log.spit(printMatrix(invariants));
 	}
-	
-	
 
-	
+
+
+
 	public int[][] getidentityMatrix(int N) {
 		int[][] identity = new int[N][N];
 		for (int i = 0; i < N; i++) {
@@ -179,7 +165,7 @@ public class RedDePetri {
 		}
 		return identity;
 	}
-	
+
 	public int[][] concatMatrix(int[][] inc, int[][] id) {
 		int N = inc.length, M = inc[0].length + id[0].length;
 		int row = 0, col = 0;
@@ -200,7 +186,7 @@ public class RedDePetri {
 		}
 		return concat;
 	}
-	
+
 	public int[][] getTranspuesta(int[][] matrix) {
 		int[][] mT = new int[matrix[0].length][matrix.length];
 		for (int i = 0; i < matrix.length; i++) {
@@ -210,7 +196,7 @@ public class RedDePetri {
 		}
 		return mT;
 	}
-	
+
 	//Getters
 	public int[] getMarcaActual() {
 		return marcaActual;
@@ -230,7 +216,7 @@ public class RedDePetri {
 			}
 			s+="]";
 		}
-		
+
 		return s;
 	}
 	public String strTranSensible() {
@@ -240,7 +226,7 @@ public class RedDePetri {
 		}
 		return v;
 	}
-	
+
 	public String strMarcaActual() {
 		String v = "";
 		for (int i = 0; i < marcaActual.length; i++) {
@@ -248,7 +234,7 @@ public class RedDePetri {
 		}
 		return v;
 	}
-	
+
 	public int[] getTranSensibilizadas() {
 		return tranSensibilizadas;
 	}

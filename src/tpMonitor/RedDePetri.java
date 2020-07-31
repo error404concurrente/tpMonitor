d5aa3e36ed3ebe6f4fabd1129d41ae94c4636452
JimenaVega <jimevega28@gmail.com>
2020-07-31 09:48:22 -0300

    prints de p invariantes

--- src/tpMonitor/RedDePetri.java
+++ src/tpMonitor/RedDePetri.java
@@ -12,22 +12,28 @@ public class RedDePetri {
 	private int [] marcaActual;
 	private long [] alfa;
 	private long [] beta;
+	private int[][] pinvariant = {{0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+								  {0,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
+								  {0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0},
+								  {1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,1,0,1,0},
+								  {0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,0,0,0,1},
+								  {0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
+								  {0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1}};
 
 
 	public RedDePetri(int[][] inc, int[] trans, int[] act, long [] alfa, long [] beta) {
-		matrizIncidencia= inc;
+		matrizIncidencia   = inc;
 		tranSensibilizadas = trans;
-		marcaInicial = act;
-		marcaActual = act;
-		this.alfa = alfa;
-		this.beta = beta;
-		
-		transTimestamp = new long[tranSensibilizadas.length];
+		marcaInicial       = act;
+		marcaActual        = act;
+		this.alfa          = alfa;
+		this.beta          = beta;
+		transTimestamp     = new long[tranSensibilizadas.length];
 		
 		for (int i = 0; i < transTimestamp .length; i++) {
 			transTimestamp[i]=System.currentTimeMillis();
 		}
-		
 	}
 
 	public void changeCurrent(int[] newCurrent) {
@@ -41,21 +47,22 @@ public class RedDePetri {
 //		Log.spit("Estado de RdP Antes:   " + strMarcaActual() + "  ----  T. Sensibles Antes:   " + strTranSensible());
 		calcularMarcaActual(hilo.getTarea());
 		calcularVectorSensible();
-		Log.spit("[Disparo Efectuado]: "+Thread.currentThread().getName()+"  [Estado Actual]: "+strMarcaActual()+" [T.Sensibilizadas]: "+strTranSensible());
+		Log.spit("[Disparo Efectuado]: " + Thread.currentThread().getName() + "  [Estado Actual]: " + strMarcaActual()
+				+ " [T.Sensibilizadas]: " + strTranSensible());
 //		Log.spit("Estado de RdP Despues: " + strMarcaActual() + "  ----  T. Sensibles Despues: " + strTranSensible());
 //		Log.spit("-------------- Fin Resultados --------------");
 		Politicas.aumentar(hilo);
 		Log.addDisparo(hilo.getTarea());
-		//showPinvariants();
-		
+		showPinvariants(pinvariant, marcaActual);
+
 	}
 
-	public boolean verificarCompatibilidad(int[] tarea,Hilo hilo) throws InterruptedException{
-		//Ej: verificarCompatibilidad(hilo.getTarea())
-		for(int i = 0; i < tranSensibilizadas.length; i++) {
-			if(tranSensibilizadas[i]==1 && tarea[i]==1){
-					if(( !hilo.getPolitico() || (hilo.getPolitico() && Politicas.decidirYo(hilo)))) {
-	//				Log.spit("RDP - Transicion detectada: T"+i);
+	public boolean verificarCompatibilidad(int[] tarea, Hilo hilo) throws InterruptedException {
+		// Ej: verificarCompatibilidad(hilo.getTarea())
+		for (int i = 0; i < tranSensibilizadas.length; i++) {
+			if (tranSensibilizadas[i] == 1 && tarea[i] == 1) {
+				if ((!hilo.getPolitico() || (hilo.getPolitico() && Politicas.decidirYo(hilo)))) {
+					// Log.spit("RDP - Transicion detectada: T"+i);
 					return true;
 				}
 			}
@@ -87,27 +94,27 @@ public class RedDePetri {
 		}
 	}
 
-	private void calcularVectorSensible(){
-		int[] s = new int[marcaActual.length];  //vector auxiliar S
+	private void calcularVectorSensible() {
+		int[] s = new int[marcaActual.length]; // vector auxiliar S
 		int[] aux = new int[tranSensibilizadas.length];
 		for (int i = 0; i < aux.length; i++) {
-			aux[i]=tranSensibilizadas[i];
+			aux[i] = tranSensibilizadas[i];
 		}
-		
-		for (int i = 0; i < matrizIncidencia[0].length; i++) { //Recorrer columnas
-			for (int j = 0; j < matrizIncidencia.length; j++) { //Recorrer filas
-				s[j] = marcaActual[j] + matrizIncidencia[j][i];	//Calculo de S
+
+		for (int i = 0; i < matrizIncidencia[0].length; i++) { // Recorrer columnas
+			for (int j = 0; j < matrizIncidencia.length; j++) { // Recorrer filas
+				s[j] = marcaActual[j] + matrizIncidencia[j][i]; // Calculo de S
 			}
-			/**Crear nuevo vector de transiciones sensibilizadas
-			 * En la posicion i va a ser 1 si no hay valores negativos en S,
-			 * de otra forma será 0
+			/**
+			 * Crear nuevo vector de transiciones sensibilizadas En la posicion i va a ser 1
+			 * si no hay valores negativos en S, de otra forma será 0
 			 */
-			
-			tranSensibilizadas[i]=1;
+
+			tranSensibilizadas[i] = 1;
 //			transTimestamp[i]=System.currentTimeMillis();
 			for (int x = 0; x < s.length; x++) {
-				if(s[x]<0){
-					tranSensibilizadas[i]=0;
+				if (s[x] < 0) {
+					tranSensibilizadas[i] = 0;
 					break;
 				}
 			}
@@ -132,11 +139,11 @@ public class RedDePetri {
 //					Log.spit("MIMIENDO");
 //					Log.spit("t:" + t + "   " + alfa[i] + "  " + System.currentTimeMillis() + "   " + transTimestamp[i]);
 					return false;
-				}else if( t<=0 && beta[i] >= tSensible) {
+				} else if (t <= 0 && beta[i] >= tSensible) {
 //					Log.spit("NO MIMIENDO: t:" + t + "   " + alfa[i] + "  " + System.currentTimeMillis() + "   "+ transTimestamp[i]);
 					return true;
-				}else if( t<=0 && beta[i] < tSensible ){
-					Log.spit(beta[i]+" ");
+				} else if (t <= 0 && beta[i] < tSensible) {
+					Log.spit(beta[i] + " ");
 //					Log.spit("Error de Beta: "+tSensible+">beta");
 					return false;
 				}
@@ -144,7 +151,7 @@ public class RedDePetri {
 		}
 		return true;
 	}
-	
+
 	public long mimirTime(Hilo hilo) {
 		int[] aux = hilo.getTarea();
 		for (int i = 0; i < aux.length; i++) {
@@ -157,28 +164,27 @@ public class RedDePetri {
 		return 1000;
 	}
 	
-	public void showPinvariants(int[][] pinvariant,int[] m) {
+	public void showPinvariants(int[][] pinvariant, int[] m) {
 		String s = "";
-		boolean first=false;
-		int k=0;
-		for(int i=0; i<pinvariant.length;i++){
+		boolean first = false;
+		int k = 0;
+		for (int i = 0; i < pinvariant.length; i++) {
 			first = true;
-			for(int j=0;j<pinvariant[0].length;j++) {
+			for (int j = 0; j < pinvariant[0].length; j++) {
 
-				if(pinvariant[i][j]!=0 && first) {
-					s+= " P"+j+"("+m[j]+") ";
+				if (pinvariant[i][j] != 0 && first) {
+					s += " P" + j + "(" + m[j] + ") ";
 					k += m[j];
 					first = false;
-				}
-				else if(pinvariant[i][j]!=0){
-					  k += m[j];
-					  s += "+  P"+j+"("+m[j]+")";
+				} else if (pinvariant[i][j] != 0) {
+					k += m[j];
+					s += " +  P" + j + "(" + m[j] + ")";
 				}
 			}
-			s+= "= "+k;
-		    Log.spit(s);
-			k=0;
-			s="";
+			s += " = " + k;
+			Log.spit(s);
+			k = 0;
+			s = "";
 		}
 	}
 

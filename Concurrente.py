ae672c546a09587d66f4cb436f29df3e6560c5ba
AlejoSev <alejoseverini@gmail.com>
2020-08-03 14:25:22 -0300

    Terminado script python, agregados invariantes al tinvariantes.txt

--- Concurrente.py
+++ Concurrente.py
@@ -1,11 +1,11 @@
-f = open("C:\Users\Alejo\Desktop\UNC\4 - Primer Semestre\Programación Concurrente\Tp_Final\tpMonitor\tinvariantes.txt","r")
+f = open("/Users/Alejo/Desktop/UNC/4 - Primer Semestre/Programación Concurrente/Tp_Final/tpMonitor/tinvariantes.txt","r")
 transInvariants0 = f.read().split("\n")
 transInvariants1 = []
 for i in transInvariants0:
     transInvariants1.append(i.split(" "))
 f.close()
 
-f = open("C:\Users\Alejo\Desktop\UNC\4 - Primer Semestre\Programación Concurrente\Tp_Final\tpMonitor\disparos.txt","r")
+f = open("/Users/Alejo/Desktop/UNC/4 - Primer Semestre/Programación Concurrente/Tp_Final/tpMonitor/disparos.txt","r")
 log = f.read().split(" ")
 f.close()
 
@@ -14,24 +14,27 @@ print(log)
 
 counter = 0
 index = []
+invCounter = 0
 
-for i in range(len(transInvariants1)):
-    for j in range(len(transInvariants1[i])):
-        for k in range(len(log)):
-            if(transInvariants1[i][j] == log[k]):
-                index.append(k)
-                counter = counter+1
-                break
-    if(counter == len(transInvariants1[i])):
-        index.reverse()
-        for i in index:
-            print("Len:",len(log))
-            log.pop(i)
-        index.clear()
-        counter = 0
-        print("done")
-    else:
-        index.clear()
-        counter = 0
+for i in range(200):
+    for i in range(len(transInvariants1)):
+        for j in range(len(transInvariants1[i])):
+            for k in range(len(log)):
+                if(transInvariants1[i][j] == log[k]):
+                    index.append(k)
+                    counter = counter+1
+                    break
+        if(counter == len(transInvariants1[i])):
+            index.reverse()
+            for i in index:
+                #print("Len:",len(log))
+                log.pop(i)
+            index.clear()
+            counter = 0
+            invCounter = invCounter+1
+        else:
+            index.clear()
+            counter = 0
 
 print(log)
+print("Cantidad de invariantes encontrados: ",invCounter)
